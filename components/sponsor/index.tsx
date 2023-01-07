a20b43a18117df0e494979a1c0071f9eb0082a2b
xxxtentacion_ol <eleven980828@gmail.com>
2023-01-08 00:50:40 +0800

    feat: 初始化首页

---
+++ components/sponsor/index.tsx
@@ -0,0 +1,67 @@
+import { FC } from "react";
+import styles from "./styles.module.scss"
+import Image from "next/image";
+import rightArrow from "@/public/rightAllow.png";
+import speaker from "@/public/test.jpg";
+import speakerBack from "@/public/SPEAKER_back.png";
+const ALLSPEAKERS=[
+    {
+        url:speaker,
+        name:"KATE",
+        postion:"NYCfounder CEO"
+    },{
+        url:speaker,
+        name:"KATE",
+        postion:"NYCfounder CEO"
+    }
+    ,{
+        url:speaker,
+        name:"KATE",
+        postion:"NYCfounder CEO"
+    },
+    {
+        url:speaker,
+        name:"KATE",
+        postion:"NYCfounder CEO"
+    },{
+        url:speaker,
+        name:"KATE",
+        postion:"NYCfounder CEO"
+    },{
+        url:speaker,
+        name:"KATE",
+        postion:"NYCfounder CEO"
+    },{
+        url:speaker,
+        name:"KATE",
+        postion:"NYCfounder CEO"
+    },{
+        url:speaker,
+        name:"KATE",
+        postion:"NYCfounder CEO"
+    }
+]
+const Sponsor: FC =({}) => {
+    return(
+        <div className={styles.sponsor}>
+                <div className={styles.title}>SPEAKER
+                <div className={styles.Apply}>Apply
+                <Image src={rightArrow}  alt="rightArrow" className={styles.rightArrow}></Image>
+                </div>
+                </div>
+                <Image src={speakerBack} alt="speakerBack" className={styles.speakerBack}></Image>
+                <ul className={styles.allspeaker}>
+                    {
+                        ALLSPEAKERS.map((item,index)=>{
+                            return <li key={index} className={styles.speakersLi}>
+                                <Image src={item.url} alt={item.name+index} className={styles.speakerImg}></Image>
+                                <span>{item.name}</span>
+                                <p>{item.postion}</p>
+                            </li>
+                        })
+                    }
+                </ul>
+        </div>
+    )
+}
+export default Sponsor;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from "next";
 import styles from "./index.module.scss";
-import Banner from '../components/banner'
-import Liangdian from '../components/Liangdian'
+import Banner from '../components/banner';
+import Liangdian from '../components/Liangdian';
+import Sponsor from "../components/sponsor";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Banner></Banner>
       <Liangdian></Liangdian>
+      <Sponsor></Sponsor>
     </div>
   );
 };
