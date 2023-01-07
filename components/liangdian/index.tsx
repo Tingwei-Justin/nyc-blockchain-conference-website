3a162feb16da12e723cef6764cd46974f3c884c2
‘Alan’ <1531638633@qq.com>
2023-01-07 21:58:22 +0800

    feat: 完善亮点组件

--- components/liangdian/index.tsx
+++ components/liangdian/index.tsx
@@ -1,8 +1,7 @@
 import { FC } from 'react';
 import styles from "./styles.module.scss"
 import Image from 'next/image';
-import clock from "@/public/clock.png"
-import address from "@/public/address.png"
+import liangdian from "@/public/liangdian.png"
 
 const Liangdian: FC = ({}) => {
   return (
@@ -18,14 +17,55 @@ const Liangdian: FC = ({}) => {
         <div className={styles.liangdianInfoItems}>
           <div className={styles.liangdianItemsItem}>
             <div className={styles.liangdianItem}>
-              <div className={styles.itemIndex}>
-                1
+              <div className={styles.itemContent}>
+                <div className={styles.itemIndex}>
+                  1
+                </div>
+                <div className={styles.itemInfo}>
+                  Co-hosted by CTH Group, a leading global blockchain ecosystem builder, 
+                  and the City of Miami, the Miami Web3 Summit will be the most significant 
+                  international institutional Web3 event of 2022 as we witness this once-in-a-generation 
+                  transformation toward our future digital economy.
+                </div>
               </div>
+              <div className={styles.itemContent}>
+                <div className={styles.itemIndex}>
+                  2
+                </div>
+                <div className={styles.itemInfo}>
+                  Co-hosted by CTH Group, a leading global blockchain ecosystem builder, 
+                  and the City of Miami, the Miami Web3 Summit will be the most significant 
+                  international institutional Web3 event of 2022 as we witness this once-in-a-generation 
+                  transformation toward our future digital economy.
+                </div>
+              </div>
+              <div className={styles.itemContent}>
+                <div className={styles.itemIndex}>
+                  3
+                </div>
+                <div className={styles.itemInfo}>
+                  Co-hosted by CTH Group, a leading global blockchain ecosystem builder, 
+                  and the City of Miami, the Miami Web3 Summit will be the most significant 
+                  international institutional Web3 event of 2022 as we witness this once-in-a-generation 
+                  transformation toward our future digital economy.
+                </div>
+              </div>
+            </div>
+            <div className={styles.liangdianBanner}>
+              <Image className={styles.liangdianImg} src={liangdian} alt="liangdianLogo"  />
             </div>
           </div>
         </div>
-        <div  className={styles.liangdianInfoItem}>
-
+        <div className={styles.liangdianInfoItem}>
+          <div className={styles.itemIndex}>
+            4
+          </div>
+          <div className={styles.liangdianInfoItemWord}>
+            Co-hosted by CTH Group, a leading global blockchain ecosystem builder, 
+            and the City of Miami, the Miami Web3 Summit will be the most significant 
+            international institutional Web3 event of 2022 as we witness this once-in-a-generation 
+            transformation toward our future digital economy.
+          </div>
         </div>
       </div>
     </div>
