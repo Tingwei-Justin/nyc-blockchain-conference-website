ec52c5a2f94f2640cfbd04fe7dff92fa3e31ffcf
‘Alan’ <1531638633@qq.com>
2023-01-08 02:18:11 +0800

    feat: 初始化首页

--- components/sponsor/index.tsx
+++ components/sponsor/index.tsx
@@ -5,6 +5,7 @@ import binance from "@/public/binance.png"
 import meta_mask from "@/public/meta_mask.png"
 import trust from "@/public/trust.png"
 import token_pocket from "@/public/token_pocket.png"
+import right_allow from "@/public/rightAllow.png"
 
 const SPONSOR_LIST = [
   binance, meta_mask, trust, token_pocket,
@@ -21,7 +22,10 @@ const Sponsor: FC = ({}) => {
         <div className={styles.title}>
           SPONSOR
         </div>
-        <div></div>
+        <div className={styles.apply}>
+          APPLY
+          <Image src={right_allow} className={styles.rightAllow} alt="right_allow"></Image>
+        </div>
       </div>
         <div className={styles.sponsorWrap}>
           {SPONSOR_LIST.map((item, index) => {
