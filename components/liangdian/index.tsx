b05a006c86f6137a10f9767ac2250b2a683a7339
Tingwei Zhou <zhoutingwei1996@gmail.com>
2023-02-08 23:25:01 -0500

    fix lint error

--- components/liangdian/index.tsx
+++ components/liangdian/index.tsx
@@ -18,28 +18,28 @@ const Liangdian: FC = ({}) => {
               <div className={styles.itemContent}>
                 <div className={styles.itemIndex}>1</div>
                 <div className={styles.itemInfo}>
-                  How to rebuild the industry consensus after the conllapse of
-                  3AC, LUNA, and FTX;
+                  {`How to rebuild the industry consensus after the conllapse of
+                  3AC, LUNA, and FTX`}
                 </div>
               </div>
               <div className={styles.itemContent}>
                 <div className={styles.itemIndex}>2</div>
                 <div className={styles.itemInfo}>
-                  To enhance dialogues between scholars, legislators, government
+                  {`To enhance dialogues between scholars, legislators, government
                   authorities and the core industry leaders. As to build mutual
                   trust through private meetings and open dialogues, and also,
                   to explore a reciprocal and applicable industry
                   self-regulatory standards and government regulatory rules.
                   Aimed to issue the "Blockchain Autonomy Declaration 2023"
-                  after the summit.
+                  after the summit.`}
                 </div>
               </div>
               <div className={styles.itemContent}>
                 <div className={styles.itemIndex}>3</div>
                 <div className={styles.itemInfo}>
-                  Promote the constitution of the metaverse and establish a more
+                  {`Promote the constitution of the metaverse and establish a more
                   orderly metaverse ecology. Promote the governance level of
-                  DAO;
+                  DAO`}
                 </div>
               </div>
             </div>
@@ -55,10 +55,10 @@ const Liangdian: FC = ({}) => {
         <div className={styles.liangdianInfoItem}>
           <div className={styles.itemIndex}>4</div>
           <div className={styles.liangdianInfoItemWord}>
-            Establish the largest industry summit brand in New York, and provide
+            {`Establish the largest industry summit brand in New York, and provide
             an effective communication platform for regulators, Wall Street,
             academia, technologists, entrepreneurs, and cryptocurrency
-            enthusiasts.
+            enthusiasts`}
           </div>
         </div>
       </div>
